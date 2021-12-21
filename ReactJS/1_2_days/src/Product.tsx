5c07d1a51f91ab9e05cc3fe75a1b4edf42560d8d
Hakan ÖZER <hakanozer02@gmail.com>
2021-12-21 10:03:46 +0300

    2 Days

--- ReactJS/1_2_days/src/Product.tsx
+++ ReactJS/1_2_days/src/Product.tsx
@@ -1,9 +1,44 @@
 import React from 'react'
 
 export default function Product() {
+
+
+    const obj = {
+        datas: [
+           { id: 10, name: "Ali", age: 30 }, 
+           { id: 12, name: "Mehmet", age: 35 },
+           { id: 14, name: "Ahmet", age: 40 },
+           { id: 16, name: "Zehra", age: 45 },
+        ]
+    }
+
+
     return (
         <div>
             <h1>Welcome Product</h1>
+            <table className="table">
+            <thead>
+                <tr>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Age</th>
+                </tr>
+            </thead>
+            <tbody>
+
+            { obj.datas.map( (item, index) => {
+                return (
+                    <tr key={index}>
+                        <th scope="row">{ item.id }</th>
+                        <td>{item.name}</td>
+                        <td>{ item.age }</td>
+                    </tr>
+                    )
+            } ) }
+                
+                
+            </tbody>
+            </table>
         </div>
     )
 }
--- ReactJS/1_2_days/src/index.tsx
+++ ReactJS/1_2_days/src/index.tsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// components
+import NavMenu from './components/NavMenu';
 
 // import pages
 import App from './App';
 import Product from './Product';
 import Detail from './Detail';
 
+
 const objRoute = 
 <Router>
+  <NavMenu />
   <Routes>
     <Route path="/" element={<App/>} ></Route>
     <Route path="/product" element={<Product/>}></Route>
@@ -20,5 +24,4 @@ const objRoute =
 
 
 ReactDOM.render( objRoute, document.getElementById('root'));
-
 reportWebVitals();
